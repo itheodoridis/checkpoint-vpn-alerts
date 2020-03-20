461d6a5b9a0b158ebc5aa890adfb91d68991dae8
Ioannis Theodoridis <15370381+itheodoridis@users.noreply.github.com>
2020-03-20 02:09:08 +0200

    Update and rename checkvals.txt to checkvals.py

--- checkvals.py
+++ checkvals.py
@@ -1,15 +1,18 @@
+#!/usr/bin/python3
 from netmiko import ConnectHandler, ssh_exception
 from paramiko.ssh_exception import SSHException
 from getpass import getpass
 import subprocess
 import sys
+import smtplib
+import time
 
 #user = input('username:')
 #passwd = getpass()
 
-user = 'cpmonitor'
-passwd = 'M0nitor'
-hostname = '10.132.1.5'
+user = 'username'
+passwd = 'password'
+hostname = 'ipaddress'
 
 mkfwext1 = {
     'device_type': 'checkpoint_gaia',
@@ -20,7 +23,7 @@ mkfwext1 = {
 
 try:
     net_connect = ConnectHandler(**mkfwext1)
-except SSHException as e:#replace with netmiko exception
+except SSHException as e:
     print("Can't connect to device {},\n{}".format(hostname, e))
     sys.exit(1)
 except ssh_exception.NetMikoTimeoutException as e:
@@ -40,5 +43,33 @@ for line in lines:
     peak = vars[4]
     print ("Current Remote Users: ",vals)
     print ("Peak number of users:", peak)
-
 net_connect.disconnect()
+
+#timestr = time.strftime("%Y%m%d_%H%M")
+timestr = str(time.strftime("%H:%M %d/%m/%Y"))
+
+recipients = "user1@domain.com,user2@domain.com,user3@domain.com "
+TO = recipients.split(',')
+SUBJECT = 'VPN users report'
+TEXT = "Date and Time: {}\nCurrent Remote Users: {}\nPeak Number of users: {}".format(timestr, vals, peak)
+
+# exchange Sign In
+exchange_sender = 'userx@domain.com'
+exchange_passwd = 'userpassword'
+
+server = smtplib.SMTP('smtpserver', 25)
+server.ehlo()
+server.starttls()
+server.login(exchange_sender, exchange_passwd)
+
+BODY = '\r\n'.join(['To: %s' % TO,
+                    'From: %s' % exchange_sender,
+                    'Subject: %s' % SUBJECT,
+                    '', TEXT])
+try:
+    server.sendmail(exchange_sender, TO, BODY)
+    print ('email sent')
+except:
+    print ('error sending mail')
+
+server.quit()
