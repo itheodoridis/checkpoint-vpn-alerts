6f6fd804a54edfb1f91d4eddaefb54217f896313
Ioannis Theodoridis <15370381+itheodoridis@users.noreply.github.com>
2020-03-21 00:04:25 +0200

    Add files via upload

---
+++ checkvpnsusers.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python3
+from netmiko import ConnectHandler, ssh_exception
+from paramiko.ssh_exception import SSHException
+from getpass import getpass
+import subprocess
+import sys
+import smtplib
+import time
+import csv
+import os
+
+#user = input('username:')
+#passwd = getpass()
+
+timestr = time.strftime("%Y%m%d")
+filename = "vpnusers_"+timestr+".csv"
+
+user = 'cpmonitor'
+passwd = 'M0nitor'
+hostname = '10.132.1.5'
+
+mkfwext1 = {
+    'device_type': 'checkpoint_gaia',
+    'ip': hostname,
+    'username': user,
+    'password': passwd,
+}
+
+try:
+    net_connect = ConnectHandler(**mkfwext1)
+except SSHException as e:#replace with netmiko exception
+    print("Can't connect to device {},\n{}".format(hostname, e))
+    sys.exit(1)
+except ssh_exception.NetMikoTimeoutException as e:
+    print("Timeout for device {},\n{}".format(hostname, e))
+    sys.exit(1)
+except ssh_exception.NetMikoAuthenticationException as e:
+    print("Invalid Credentials for device {},\n{}".format(hostname, e))
+    sys.exit(1)
+
+output = net_connect.send_command("fw tab -t userc_users -s")
+lines = output.split('\n')
+for line in lines:
+    if 'NAME' in line:
+        continue
+    vars = line.split()
+    vals = vars[3]
+    peak = vars[4]
+    print ("Current Remote Users: ",vals)
+    print ("Peak number of users:", peak)
+net_connect.disconnect()
+
+#timestr = time.strftime("%Y%m%d_%H%M")
+timestr = str(time.strftime("%H:%M %d/%m/%Y"))
+
+recipients = "itheodoridis@bankofgreece.gr,kdardoufa@bankofgreece.gr,itheodori@gmail.com,hmavroidis@bankofgreece.gr,noc@bankofgreece.gr"
+TO = recipients.split(',')
+SUBJECT = 'VPN users report'
+TEXT = "Date and Time: {}\nCurrent Remote Users: {}\nPeak Number of users: {}".format(timestr, vals, peak)
+
+# exchange Sign In
+exchange_sender = 'nwmonitor@bankofgreece.gr'
+exchange_passwd = 'Q;Q6d!-o7-;Xp{;g[)eh'
+
+server = smtplib.SMTP('smtp-mk.bankofgreece.gr', 25)
+server.ehlo()
+server.starttls()
+server.login(exchange_sender, exchange_passwd)
+
+BODY = '\r\n'.join(['To: %s' % TO,
+                    'From: %s' % exchange_sender,
+                    'Subject: %s' % SUBJECT,
+                    '', TEXT])
+try:
+    server.sendmail(exchange_sender, TO, BODY)
+    print ('email sent')
+except:
+    print ('error sending mail')
+
+server.quit()
+
+if os.path.isfile(filename):
+    vpnusersfile = open(filename, 'a')
+    csvwriter = csv.writer(vpnusersfile)
+else:
+    vpnusersfile = open(filename, 'w')
+    csvwriter = csv.writer(vpnusersfile)
+    header = ["Time", "VpnUsers", "PeakUsers"]
+    csvwriter.writerow(header)
+
+hrsandmins =  str(time.strftime("%H:%M"))
+new_row = [hrsandmins, vals, peak]
+csvwriter.writerow(new_row)
+vpnusersfile.close()
